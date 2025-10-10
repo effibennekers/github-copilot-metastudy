fa4697e750234de75167a68ed51f04a25b6aa299
effibennekers <effi@bennekers.com>
2025-10-10 16:15:03 +0200

    db: verwijder SchemaManager en schema.py; rely op ensure_* per repo

--- src/database/database.py
+++ src/database/database.py
@@ -2,17 +2,15 @@
 
 from typing import List, Dict, Optional
 
-from .schema import SchemaManager
 from .metadata_repo import MetadataRepository
 from .papers_repo import PapersRepository
 from .labels_repo import LabelsRepository
 from .queues import QueuesRepository
 
 
-class PaperDatabase(SchemaManager, MetadataRepository, PapersRepository, LabelsRepository, QueuesRepository):
+class PaperDatabase(MetadataRepository, PapersRepository, LabelsRepository, QueuesRepository):
     def __init__(self):
         super().__init__()
-        self.init_database()
         # Zorg dat alle tabellen per repository bestaan
         self.ensure_metadata_tables()
         self.ensure_papers_tables()
